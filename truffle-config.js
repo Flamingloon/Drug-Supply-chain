a402341c5125c72c3c796d1200166451f6f86a7a
Sahil Totala <81063988+Flamingloon@users.noreply.github.com>
2022-11-04 15:01:50 +0530

    Full Commit

---
+++ truffle-config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  networks: {
+    development: {
+      host: "127.0.0.1",
+      port: 8545,
+      network_id: "*",
+      gas: 30000000,
+      from: '0xaA87ECC9ed0393D7c59c5dd5110Fa4AB5108019C',
+    },
+  },
+  contracts_directory: './testing/',
+  contracts_build_directory: './src/build/',
+  compilers: {
+    solc: {
+      version: "0.6.6",
+      settings: {
+        optimizer: {
+          enabled: true,
+          runs: 100
+        }
+      }
+    }
+  }
+}
